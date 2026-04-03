12411915dbb329b3fac3e4de7a4c3209e4754f3d
nicholasanyona-star <nicholasanyona@gmail.com>
2026-04-03 12:00:10 +0300

    Create Receiving Officer Dashboard views

---
+++ receiving_officer.py
@@ -0,0 +1,14 @@
+# Receiving Officer Dashboard Views
+
+class ReceivingOfficerDashboard:
+    def __init__(self):
+        self.title = "Receiving Officer Dashboard"
+        self.widgets = []
+        # Initialize additional attributes
+
+    def add_widget(self, widget):
+        self.widgets.append(widget)
+
+    def render(self):
+        # Placeholder for rendering dashboard
+        pass
